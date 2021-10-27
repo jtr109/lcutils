feeed18d874962023759e8c3090089a5538cfb8e
Ryan Li <conbas2019@gmail.com>
2021-10-27 15:48:42 +0800

    feat: add list node

---
+++ list-node.go
@@ -0,0 +1,33 @@
+package lcutils
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func ConvertListNodeToArray(head *ListNode) []int {
+	current := head
+	result := []int{}
+	for current != nil {
+		result = append(result, current.Val)
+		current = current.Next
+	}
+	return result
+}
+
+func ConvertArrayToListNode(array []int) *ListNode {
+	if len(array) == 0 {
+		return nil
+	}
+	head := &ListNode{
+		Val: array[0],
+	}
+	current := head
+	for i := 1; i < len(array); i++ {
+		current.Next = &ListNode{
+			Val: array[i],
+		}
+		current = current.Next
+	}
+	return head
+}
