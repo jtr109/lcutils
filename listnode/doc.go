e76063aebd7daca94a22c44bb49e51854cc8530c
Ryan Li <conbas2019@gmail.com>
2021-10-27 16:28:02 +0800

    feat: update doc

--- listnode/doc.go
+++ listnode/doc.go
@@ -3,6 +3,19 @@
 // Example Usage
 //
 // ```go
-// import "github.com/lcutils/listnode"
+// package example
+//
+// import (
+// 	"testing"
+//
+// 	"github.com/jtr109/lcutils/listnode"
+// 	"github.com/stretchr/testify/assert"
+// )
+//
+// func main() {
+//   head := []int{1, 2, 3, 4}
+//   node := listnode.ConvertArrayToListNode(head) // *listnode.ListNode
+//   newHead := listnode.ConvertListNodeToArray(node) // []int{1, 2, 3, 4}
+// }
 // ```
 package listnode
