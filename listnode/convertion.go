898b764d70f0bb28fa4ef9c5f2f9dc4931b074ee
Ryan Li <conbas2019@gmail.com>
2021-11-24 14:05:35 +0800

    doc: move document

--- listnode/convertion.go
+++ listnode/convertion.go
@@ -1,5 +1,12 @@
 package listnode
 
+// Create a pointer of ListNode from a slice contains integer.
+//
+// Refers to the problem Remove Linked List Elements: https://leetcode.com/problems/remove-linked-list-elements/.
+//
+// We can follow the code below to create a ListNode from the slice:
+//
+//     listnode.FromSlice([]int{1, 2, 3, 4}) // *listnode.ListNode
 func FromSlice(s []int) *ListNode {
 	if len(s) == 0 {
 		return nil
--- listnode/doc.go
+++ listnode/doc.go
@@ -1,18 +1,2 @@
 // Package listnode provides a set of tools for writing LeetCode problems locally.
-//
-// Example Usage
-//
-// ```go
-// package example
-//
-// import "github.com/jtr109/lcutils/listnode"
-//
-// type ListNode = listnode.ListNode // type alias
-//
-// func main() {
-//   head := []int{1, 2, 3, 4}
-//   node := listnode.ConvertArrayToListNode(head) // *listnode.ListNode
-//   newHead := listnode.ConvertListNodeToArray(node) // []int{1, 2, 3, 4}
-// }
-// ```
 package listnode
