00783cfe494ddbeef45319e2a9565f1c4fa986ed
Ryan Li <conbas2019@gmail.com>
2021-11-24 10:56:24 +0800

    feat: add equal

--- treenode/structs.go
+++ treenode/structs.go
@@ -18,3 +18,19 @@ func NewTreeNode(val *nilInt) *TreeNode {
 		Val: val.Val(),
 	}
 }
+
+func treeNodeEqual(node1, node2 *TreeNode) bool {
+	if node1 == nil && node2 == nil {
+		return true
+	}
+	if node1 == nil || node2 == nil {
+		return false
+	}
+	if node1.Val != node2.Val {
+		return false
+	}
+	if !treeNodeEqual(node1.Left, node2.Left) || !treeNodeEqual(node1.Right, node2.Right) {
+		return false
+	}
+	return true
+}
